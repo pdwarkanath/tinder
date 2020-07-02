8b6f64ab9d793bfe7000ee4225a6f3fc75ac420e
Dwarkanath Prabhu <pdwarkanath@users.noreply.github.com>
2020-07-02 00:02:38 -0500

    Update tinder.py

--- tinder.py
+++ tinder.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
-# x = pyautogui.position()
-x = (930, 597)
+x = pyautogui.position() # position of like button
+# x = (930, 597)
 
 for i in range(100):
 	time.sleep(0.2)
